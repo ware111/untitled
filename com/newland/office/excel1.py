b406365dcfff58ff971f0ce1feb00e0effff5467
JavaPan <l>
2019-05-12 07:28:08 +0800

    罗列

---
+++ com/newland/office/excel1.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen as open
+from tkinter import Tk
+import win32com.client as win32
+
+u = open("http://n.sinaimg.cn/news/1_img/dfic/34fa2aa3/62/w1024h638/20190510/4350-hwsffzc2527481.jpg")
+x1= win32.gencache.EnsureDispatch('Excel.Application')
+ss = x1.Workbooks.Add()
+sh = ss.ActiveSheet
+x1.Visible = True
+sh.Cells(5,10).Value = u
