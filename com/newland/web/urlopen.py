b3b9c385bb941e05bfff020e11260061d1547794
JavaPan <l>
2019-05-12 19:45:15 +0800

    网络编程

---
+++ com/newland/web/urlopen.py
@@ -0,0 +1,3 @@
+from urllib.request import *
+
+image = urlopen("http://n.sinaimg.cn/mil/8_img/upload/21a18b46/298/w1618h1080/20190511/2214-hwsffzc4528902.jpg")
---
+++ com/newland/web/urlparse.py
@@ -0,0 +1,6 @@
+import urlparser.urlparser as url
+import urlparse2.urlparse2 as url2
+
+u = url.urlparse("https://blog.csdn.net/licheetools/article/details/82946350")
+
+
